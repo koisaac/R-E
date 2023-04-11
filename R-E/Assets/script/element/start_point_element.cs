f4945e00d8e68b50c78962a20c1b788ea6510c76
koisaac <rhdltkr.e@gmail.com>
2023-04-11 20:51:18 +0900

    버튼을 통한 제어 추가

---
+++ R-E/Assets/script/element/start_point_element.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class start_point_element : MonoBehaviour
+{
+    public float sped_x, sped_z; //자동차 이동 방향 설정
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ R-E/Assets/script/set/set_car.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class set_car : MonoBehaviour
+{
+    public List<GameObject> start_point; //시작 위치를 알려주는 오브젝트
+    public GameObject car; //프리팹
+    public int number_of_cars; //자동차 생성 갯수
+    public List<GameObject> cars; //자동차 객체 저장하는 변수
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i=0;i<number_of_cars;i++) {
+            cars.Add(Instantiate(car));//자동차 생성
+            cars[i].transform.position = start_point[i].transform.position;//자동차 위치 설정
+            cars[i].GetComponent<move>().car_sped_x = start_point[i].GetComponent<start_point_element>().sped_x;//자동차 이동방향 설정
+            cars[i].GetComponent<move>().car_sped_z = start_point[i].GetComponent<start_point_element>().sped_z;//자동차 이동방향 설정
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
