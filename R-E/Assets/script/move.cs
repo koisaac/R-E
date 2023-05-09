90b4099837f7039bca0be602f81567aafde86fbd
koisaac <rhdltkr.e@gmail.com>
2023-05-09 17:01:41 +0900

    move 오류 수정

--- R-E/Assets/script/move.cs
+++ R-E/Assets/script/move.cs
@@ -6,10 +6,16 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float car_sped_x,car_sped_z;
-    private float car_sped_x_p,car_sped_z_p;
+    private float car_sped_x_p=0,car_sped_z_p=0;
+    void OnCollisionEnter(Collision collision)
+    {
+        car_sped_x_p = car_sped_x; //이동 방향 초기화
+        car_sped_z_p = car_sped_z;
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+    }
     void OnTriggerStay(Collider other)
     {
-
+        
             Debug.Log(other.transform.Find("signal_light").GetComponent<signal_light_element>().signal);//현재 자동차가 인식하고 있는 신호 출력
             if (other.transform.Find("signal_light").GetComponent<signal_light_element>().signal == "red")//빨간 불이면 정지 아니면 움직이기
             {
@@ -25,13 +31,12 @@ public class move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        car_sped_x_p = car_sped_x; //이동 방향 초기화
-        car_sped_z_p = car_sped_z;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.Translate(new Vector3(car_sped_x_p, 0, car_sped_z_p));//이동
+        transform.Translate(new Vector3(car_sped_x_p,0,car_sped_z_p));//이동
     }
 }
