9e33723c9c3aebda7285610345130c5886440598
koisaac <rhdltkr.e@gmail.com>
2023-05-16 20:44:14 +0900

    .

--- R-E/Assets/text/test.py
+++ R-E/Assets/text/test.py
@@ -3,6 +3,8 @@ import json_manger
 
 
 jn = json_manger.json_manger()
-while(bool(input("종료할 경우 -1 입력")!="-1" if True else False)):
+while(True):
     signal = input("변경할 신호등 번호, 신호을 공백으로 구분하여 입력>>").split(' ')
     jn.change(signal[0],signal[1])
+    if(bool(input("종료할 경우 -1 입력")!="-1" if True else False)):
+        break
