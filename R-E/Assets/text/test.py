735c37fff792a20834373db3ebf44f71e9ba0283
koisaac <rhdltkr.e@gmail.com>
2023-05-30 15:42:26 +0900

    몰?루

--- R-E/Assets/text/test.py
+++ R-E/Assets/text/test.py
@@ -6,5 +6,5 @@ jn = json_manger.json_manger()
 while(True):
     signal = input("변경할 신호등 번호, 신호을 공백으로 구분하여 입력>>").split(' ')
     jn.change(signal[0],signal[1])
-    if(bool(input("종료할 경우 -1 입력")!="-1" if True else False)):
+    if(bool(input("종료할 경우 -1 입력")=="-1" if True else False)):
         break
