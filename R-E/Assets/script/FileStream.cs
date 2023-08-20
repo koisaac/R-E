8bbc496880d84a8997ac248b7468ce5819c033d6
koisaac <rhdltkr.e@gmail.com>
2023-08-20 16:55:11 +0900

    filestream.cs추가

--- R-E/Assets/script/FileStream.cs
+++ R-E/Assets/script/FileStream.cs
@@ -1,18 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json;
+using System.IO;
+using System.Runtime.CompilerServices;
 
-public class FileStream : MonoBehaviour
+[System.Serializable]
+public class SignalData
 {
-    // Start is called before the first frame update
-    void Start()
+    public string signal;
+    public bool is_turn_left;
+}
+
+class FileStream : MonoBehaviour
+{
+
+    public string json_filePath;
+    private const string SignalData = "Signal"; 
+
+    private static FileStream filestream = null;
+    void Awake()
     {
-        
+        if (filestream == null)
+        {
+            filestream = this;
+        }
+        else if (filestream != this)
+        {
+            Destroy(this);
+        }
+    }
+    public static FileStream Instance
+    {
+        get
+        {
+            if (filestream == null)
+            {
+                return null;
+            }
+            return filestream;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public static string signaldata => SignalData;
+
+    public string GetFile_string(string path)
     {
-        
+        return File.ReadAllText(path);
+    }
+
+    public List<SignalData> GetFile_SignalData(string path)
+    {
+        switch (path)
+        {
+            case "Signal":
+                return JsonConvert.DeserializeObject<List<SignalData>>(GetFile_string(json_filePath));
+            default:
+                return JsonConvert.DeserializeObject<List<SignalData>>(GetFile_string(path));
+        }
     }
 }
